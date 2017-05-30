de2832962e0b533b90ab4e8e0c353c64597899f2
promilo <milind.shah@mail.utoronto.ca>
2017-05-30 20:49:17 +0000

    Able to print every message

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
 import React, {Component} from 'react';
 
 import ChatBar from './ChatBar.jsx';
-import Message from './Message.jsx';
-import MessageList from './Message.jsx';
+import MessageList from './MessageList.jsx';
+
+const data = {
+    currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+    messages: [
+    {
+      username: "Bob",
+      content: "Has anyone seen my marbles?",
+    },
+    {
+      username: "Anonymous",
+      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+    }
+  ]
+}
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = data;
+  }
+
+
+
+
+
+
   render() {
+    console.log("Rendering <App/>");
     return (
       <div>
-        <MessageList />
-        <ChatBar />
+        <MessageList msgs = {this.state.messages} />
+        <ChatBar user={this.state.currentUser.name} />
+
       </div>
     );
   }
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -2,16 +2,22 @@ import React, {Component} from 'react';
 
 
 
-import Message from '/.Message.jsx';
+import Message from './Message.jsx';
 
 
 class MessageList extends Component {
   render() {
+    console.log("Rendering MessageList");
     return (
         <div>
-        <Message />
+          {this.props.msgs.map(this._printMsg)}
+
         </div>
     );
   }
+  _printMsg = (msg) => {
+    return <Message username = {msg.username} content = {msg.content}/>
+  }
+
 }
 export default MessageList;
