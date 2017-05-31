7ba827807b84507f3c2dba072e7d1193d9c730c8
promilo <milind.shah@mail.utoronto.ca>
2017-05-31 23:20:10 +0000

    Added realtime chat functionality

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
 
+const uuidV4 = require('uuid/v4');
+
 // Set the port to 3001
 const PORT = 3001;
 
@@ -15,15 +17,39 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
+
+
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
+//ws is the (client)
 wss.on('connection', (ws) => {
   console.log('Client connected');
+
+
+
+  const broadcast = (message) => {
+    wss.clients.forEach((c) => {
+      if(c != ws) {
+        c.send(JSON.stringify(message));
+      }
+    });
+  }
+
+
+
+
   ws.on('message', function incoming(message) {
     console.log("input is", message);
     console.log('received: %s', JSON.parse(message));
+    let theMessage = JSON.parse(message);
+    theMessage.key = uuidV4();
+    console.log(theMessage);
+    broadcast(theMessage);
   });
+
+
+
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
