089425fb5c84c00f72e99b78573a3b82457245ae
promilo <milind.shah@mail.utoronto.ca>
2017-05-31 19:12:54 +0000

    Successfully made a connection to send data to the server

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -20,6 +20,10 @@ const wss = new SocketServer({ server });
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  ws.on('message', function incoming(message) {
+    console.log("input is", message);
+    console.log('received: %s', JSON.parse(message));
+  });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
